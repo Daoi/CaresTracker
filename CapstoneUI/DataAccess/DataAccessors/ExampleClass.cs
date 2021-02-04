993d671550518cb48ad27958de95f5f01b9f6853
Daoi <klynch0092@gmail.com>
2021-02-03 21:35:39 -0500

    comments

--- CapstoneUI/DataAccess/DataAccessors/ExampleClass.cs
+++ CapstoneUI/DataAccess/DataAccessors/ExampleClass.cs
@@ -9,13 +9,33 @@ namespace CapstoneUI.DataAccess.DataAccessors
 {
     public class ExampleClass : DataSupport, IData
     {
+        /// <summary>
+        /// Important Note:
+        /// Using this method of accessing the database, it's ideal to have
+        /// a class for each stored procedure.
+        /// e.g. this class would be the "GetUserCommandClass" (probably with a better name)
+        /// This way, whenever theres a problem we know exactly where to look and avoid issues with
+        /// multiple commands originating from the same class. If a command requires something special
+        /// it can be handled in its own class. e.g. if userinput is formatted differently then its stored in the db
+        /// Could handle the formatting here in a method, or maybe ideally that would have its own class too
+        /// </summary>
+
+        //A constructor to set some defaults that will be the same everytime we use this class(i.e. the command name and type)
         public ExampleClass()
         {
-            ConnectionString = GetConnectionString("dbConnectionString1");
+            //ConnectionString = GetConnectionString("dbConnectionString");
             CommandText = "GetUserExample";
             CommandType = CommandType.StoredProcedure;
         }
-
+        /// <summary>
+        /// Create the parameters for the SQL Stored procedure. The procedure has two variables, @UserAge and @TestResult
+        /// These variables values are passed in, but can be set in the class or where ever it makes sense to
+        /// After that, we create an EQ Object, and then call the appropriate sql method. ExecuteAdapter or ExecuteScalar would
+        /// work here, execute scalar is probably better for a situation where you only expect one row/item to be returned but oh well
+        /// </summary>
+        /// <param name="age"></param>
+        /// <param name="testResult"></param>
+        /// <returns></returns>
         public DataTable LookUpUser(int age, bool testResult)
         {
             Parameters = new SqlParameter[2] { new SqlParameter("UserAge", age), new SqlParameter("TestResult", testResult) };
