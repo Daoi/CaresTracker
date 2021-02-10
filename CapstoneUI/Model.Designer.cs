7f3619b9f101e2ec50d19273a55a9bdf8c88fdde
Daoi <klynch0092@gmail.com>
2021-02-10 14:07:03 -0500

    entity cares user

--- CapstoneUI/Model.Designer.cs
+++ CapstoneUI/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Tyler Hachadorian\Documents\GitHub\CovidTracking\CapstoneUI\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Mrah\source\repos\CaresTracker\CapstoneUI\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
