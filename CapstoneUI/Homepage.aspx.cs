e8e247c835c5b62979c569fdce24c9cbc8e959be
Daoi <klynch0092@gmail.com>
2021-02-17 00:29:15 -0500

    uncommenting session handling

--- CapstoneUI/Homepage.aspx.cs
+++ CapstoneUI/Homepage.aspx.cs
@@ -7,13 +7,13 @@ namespace CapstoneUI
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //lblUserInfo.Text = $"Welcome, {Session["UserName"].ToString()}";
+            lblUserInfo.Text = $"Welcome, {Session["UserName"].ToString()}";
             lblUserInfo.Visible = true;
             lblUserInfo.Enabled = true;
-            //if ((int)Session["AccountType"] == 0)
-            //{
-            //    divCreateCHW.Visible = false;
-            //}
+            if ((int)Session["AccountType"] == 0)
+            {
+                divCreateCHW.Visible = false;
+            }
         }
 
         protected void btnCreateResidentProfile_Click(object sender, EventArgs e)
