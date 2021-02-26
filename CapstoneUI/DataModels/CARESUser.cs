a1166bfc7fc99fbcdad6a7de752adad03cd99ca0
Wilson Diaz <wdfiguereo@gmail.com>
2021-02-26 18:52:40 -0500

    update properties

--- CapstoneUI/DataModels/CARESUser.cs
+++ CapstoneUI/DataModels/CARESUser.cs
@@ -9,12 +9,11 @@ namespace CapstoneUI.DataModels
     {
         public int UserID { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserEmail { get; set; }
         public string UserPhoneNumber { get; set; }
-        public DateTime LastLogin { get; set; }
+        public string LastLogin { get; set; }
         public string UserStatus { get; set; }
         public string UserType { get; set; }
         public string Supervisor { get; set; }
