47b5db6e23e89cf1baf0dd8cbcff5dc1fff7fd94
Daoi <klynch0092@gmail.com>
2021-03-04 15:46:30 -0500

    create session objects/sql refactor
    DT generation now entirely done in SQL instead of mix of SQL/C#

--- CapstoneUI/InteractionList.aspx.cs
+++ CapstoneUI/InteractionList.aspx.cs
@@ -8,46 +8,59 @@ using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using CapstoneUI.DataAccess.DataAccessors;
+using CapstoneUI.DataAccess;
 
 namespace CapstoneUI
 {
     public partial class InteractionList : Page
     {
         CARESUser user;
+        DataTable dt;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+
             if (!IsPostBack)
             {
-                if (HttpContext.Current.Request.Url.ToString().Contains("ResidentProfile"))
+                try
+                {
+                    user = Session["User"] as CARESUser;
+                }
+                catch(Exception ex)
+                {
+                    
+                    Response.Redirect("Login.aspx");
+                }
+
+
+                if (Session["Resident"] != null && HttpContext.Current.Request.Url.ToString().Contains("ResidentProfile"))
                 {
                     Resident res = Session["Resident"] as Resident;
                     hfResidentDetails.Value = $"{res.FirstName} {res.LastName} {res.Home.Address}";
                 }
 
-                user = Session["User"] as CARESUser;
-                GetAllInteractions getAllInteractions = new GetAllInteractions();
-                DataTable ds = getAllInteractions.ExecuteCommand();
 
                 if (user.UserType == "C")
                 {
-                    for (int i = ds.Rows.Count - 1; i >= 0; i--)
-                    {
-                        DataRow record = ds.Rows[i];
-                        if (int.Parse(record["HealthWorkerID"].ToString()) != user.UserID)
-                        {
-                            ds.Rows[i].Delete();
-                        }
-                    }
-                    ds.AcceptChanges();
-                    gvInteractionList.Columns[3].Visible = false;
-                    gvInteractionList.DataSource = ds;
+                    GetAllInteractionsByWorkerID gi = new  GetAllInteractionsByWorkerID();
+                    dt = gi.RunCommand(user.UserID);
+                    //Remove the CHW name columns as they can only see their own
+                    dt.Columns.Remove("FirstName1");
+                    dt.Columns.Remove("LastName1");
+
+                    gvInteractionList.DataSource = dt;
+
                 }
                 else
                 {
-                    gvInteractionList.DataSource = ds;
+                    //Need to add organization filtering
+                    GetAllInteractions getAllInteractions = new GetAllInteractions();
+                    dt = getAllInteractions.ExecuteCommand();
+                    gvInteractionList.DataSource = dt;
                 }
 
-                if (ds.Rows.Count != 0)
+                if (dt.Rows.Count != 0)
                 {
                     //Adds table head tag to visual studio html output so gridview format will work DataTables
                     gvInteractionList.DataBound += (object o, EventArgs ev) =>
@@ -56,8 +69,12 @@ namespace CapstoneUI
                     };
                 }
 
+                Session["InteractionListDT"] = dt;
                 gvInteractionList.DataBind();
             }
+
+            if (Session["InteractionListDT"] != null)
+                dt = Session["InteractionListDT"] as DataTable;
         }
 
 
@@ -71,6 +88,33 @@ namespace CapstoneUI
         protected void btnViewInteraction_Click(object sender, EventArgs e)
         {
 
+            if (Session["InteractionListDT"] != null)
+               dt = Session["InteractionListDT"] as DataTable;
+            else
+            {
+                //Error handling
+            }
+
+            Button btn = (Button)sender;
+            GridViewRow row = (GridViewRow)btn.NamingContainer;
+            //Recreate the Datarow the GVR is bound to
+            DataRow dr = dt.Rows[row.DataItemIndex];
+
+
+
+            //Create resident
+            GetResidentByID gr = new GetResidentByID();
+            DataRow resDr = gr.RunCommand(int.Parse(dr["ResidentID"].ToString())).Rows[0];
+            Resident res = new Resident(resDr);
+
+            Session["Resident"] = res;
+
+            //Create Interaction
+            Session["Interaction"] = new Interaction();
+
+
+
+            Server.Transfer("ResidentInteractionForm.aspx");
         }
     }
 }
