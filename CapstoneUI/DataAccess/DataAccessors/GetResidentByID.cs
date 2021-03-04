2cf43a63081744bee794873cc7ba48a2a6653cdb
Daoi <klynch0092@gmail.com>
2021-03-04 15:44:20 -0500

    new data accessors
    Interactions by worker ID and Residents by ID

---
+++ CapstoneUI/DataAccess/DataAccessors/GetResidentByID.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using MySql.Data.MySqlClient;
+namespace CapstoneUI.DataAccess
+{
+    public class GetResidentByID : DataSupport, IData
+    {
+
+        public GetResidentByID()
+        {
+            CommandText = "GetResidentByID";
+            CommandType = CommandType.StoredProcedure;
+        }
+        /// <summary>
+        /// Get the resident with the provided ID
+        /// </summary>
+        /// <param name="residentID">ID(pk) of the resident</param>
+        /// <returns></returns>
+        public DataTable RunCommand(int residentID)
+        {
+            Parameters = new MySqlParameter[1] { new MySqlParameter("ResidentID", residentID) };
+            ExecuteQuery eq = new ExecuteQuery();
+            return eq.ExecuteAdapter(this);
+        }
+    }
+}
