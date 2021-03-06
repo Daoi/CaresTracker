8ea3152c6ce46ae6d19589888613c90b9622cdfa
Daoi <klynch0092@gmail.com>
2021-03-05 23:47:48 -0500

    Data model constructors
    Create Symptom and Service objects and format them into list for use in the interaction object

--- CapstoneUI/DataModels/Service.cs
+++ CapstoneUI/DataModels/Service.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -9,5 +10,31 @@ namespace CapstoneUI.DataModels
     {
         public int ServiceID { get; set; }
         public string ServiceName { get; set; }
+        public bool IsRequested { get; set; }
+
+        public Service() { }
+
+        public Service(DataRow dr)
+        {
+            ServiceID = int.Parse(dr["ServiceID"].ToString());
+            ServiceName = dr["ServiceName"].ToString();
+            IsRequested = (bool)dr["IsRequested"];
+        }
+
+        public static (List<Service> requested, List<Service> offered) CreateServiceLists(DataTable dt)
+        {
+            (List<Service> requested, List<Service> offered) services;
+
+            services.requested = dt.Rows.OfType<DataRow>().Where(dr => (bool)dr["IsRequested"])
+                .Select(dr => new Service(dr))
+                .ToList();
+
+            services.offered = dt.Rows.OfType<DataRow>().Where(dr => !(bool)dr["IsRequested"])
+                .Select(dr => new Service(dr))
+                .ToList();
+  
+            return services;
+        }
+
     }
 }
