3add48ed804f64fd70a52981dd4a67413954440e
Daoi <klynch0092@gmail.com>
2021-03-08 18:55:08 -0500

    should fix db submission for hcv

--- CapstoneUI/CreateResident.aspx.cs
+++ CapstoneUI/CreateResident.aspx.cs
@@ -59,7 +59,6 @@ namespace CapstoneUI
             {
                 residentHouse.HouseType = "Housing Choice Voucher";
                 residentHouse.RegionID = Int32.Parse(ddlRegion.SelectedValue); // Requires validation to ensure input is a number
-                residentHouse.DevelopmentID = -1;
             }
             else
             {
