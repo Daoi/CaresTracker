ac467d32eb3cd3da7c8eb8c73182446fd0db3a28
Daoi <klynch0092@gmail.com>
2021-03-14 19:15:27 -0400

    New accessors for updating

---
+++ CapstoneUI/DataAccess/DataAccessors/InteractionAccessors/UpdateInteraction.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using CapstoneUI.DataModels;
+
+
+namespace CapstoneUI.DataAccess.DataAccessors.InteractionAccessors
+{
+    public class UpdateInteraction : DataSupport, IData
+    {
+        Interaction interaction;
+        public UpdateInteraction(Interaction interaction)
+        {
+            Parameters = new InteractionUpdateParameters().Fill(interaction);
+            CommandText = "UpdateInteraction";
+            CommandType = CommandType.StoredProcedure;
+            this.interaction = interaction;
+        }
+        /// <summary>
+        /// Update an interaction excluding Follow up status
+        /// </summary>
+        /// <returns></returns>
+        public int ExecuteCommand()
+        {
+            ExecuteQuery eq = new ExecuteQuery();
+            return eq.ExecuteNonQuery(this);
+        }
+
+        private void UpdateInteractionSymptoms()
+        {
+            
+        }
+    }
+}
