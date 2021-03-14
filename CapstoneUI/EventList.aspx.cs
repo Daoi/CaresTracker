6a41c73089d4d98de3ed7d45ed585d293860cf4a
Wilson Diaz <wdfiguereo@gmail.com>
2021-03-14 01:12:40 -0500

    populate gv with live database data

--- CapstoneUI/EventList.aspx.cs
+++ CapstoneUI/EventList.aspx.cs
@@ -4,52 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using CapstoneUI.DataAccess.DataAccessors.EventAccessors;
 
 namespace CapstoneUI
 {
     public partial class EventList : System.Web.UI.Page
     {
+        DataTable dtEventList;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            List<EventInteraction> temp = new List<EventInteraction>();
-            EventInteraction eventInteraction = new EventInteraction("The Importance of Masks", "Explaining how masks help control the spread of COVID-19", "Seminar", "Community Center 10", "12/1/2020 5:00PM", "12/1/2020 6:00PM");
-            temp.Add(eventInteraction);
-            eventInteraction = new EventInteraction("How To: Social Distancing", "Tips on staying connected from afar", "Instructional", "Virtual", "12/2/2020 3:00PM", "12/2/2020 4:00PM");
-            temp.Add(eventInteraction);
-            for (int i = 0; i < 10; i++)
+            if (!IsPostBack)
             {
-                EventInteraction tempInteraction = new EventInteraction();
-                temp.Add(tempInteraction);
+                gvEventList.DataBound += (object o, EventArgs ev) =>
+                {
+                    gvEventList.HeaderRow.TableSection = TableRowSection.TableHeader;
+                };
+
+                dtEventList = new GetAllEvents().ExecuteCommand();
+
+                if (dtEventList.Rows.Count == 0) { return; }
+                gvEventList.DataSource = dtEventList;
+                gvEventList.DataBind();
+
+                Session["EventListDT"] = dtEventList;
             }
 
-            gvEventList.DataBound += (object o, EventArgs ev) =>
-            {
-                gvEventList.HeaderRow.TableSection = TableRowSection.TableHeader;
-            };
-
-            gvEventList.DataSource = temp;
-            gvEventList.DataBind();
-        }
-
-        public class EventInteraction
-        {
-            public string EventName { get; set; }
-            public string EventDescription { get; set; }
-            public string EventType { get; set; }
-            public string EventLocation { get; set; }
-            public string EventStartDateTime { get; set; }
-            public string EventEndDateTime { get; set; }
-            public EventInteraction() { }
-            public EventInteraction(string eventname, string eventdescription, string eventtype,
-                string eventlocation, String eventstartdatetime, string eventenddatetime)
-            {
-                EventName = eventname;
-                EventDescription = eventdescription;
-                EventType = eventtype;
-                EventLocation = eventlocation;
-                EventStartDateTime = eventstartdatetime;
-                EventEndDateTime = eventenddatetime;
-            }
+            dtEventList = Session["EventListDT"] as DataTable;
         }
 
         protected void gvEventList_RowCommand(object sender, GridViewCommandEventArgs e)
