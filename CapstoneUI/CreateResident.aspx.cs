2a86be802e2d6ecdb882bed3f8bc38a985442f6e
Wilson Diaz <wdfiguereo@gmail.com>
2021-03-21 19:44:12 -0400

    refactor: implement new development filtering

--- CapstoneUI/CreateResident.aspx.cs
+++ CapstoneUI/CreateResident.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using CapstoneUI.DataAccess.DataAccessors;
-using CapstoneUI.DataAccess.DataAccessors.Examples;
+using CapstoneUI.DataAccess.DataAccessors.DevelopmentAccessors;
 using CapstoneUI.DataModels;
 
 namespace CapstoneUI
@@ -15,13 +15,15 @@ namespace CapstoneUI
     public partial class CreateResident : System.Web.UI.Page
     {
         DataTable developmentDT;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
-                // Get list of all developments
-                GetAllDevelopments GAD = new GetAllDevelopments();
-                DataTable developmentDT = GAD.ExecuteCommand();
+                // Get list of developments
+                CARESUser user = Session["User"] as CARESUser;
+                DataTable developmentDT = new GetDevelopmentsByUserID().ExecuteCommand(user.UserID);
+
                 // Bind to drop down list
                 ddlDevelopments.DataSource = developmentDT;
                 ddlDevelopments.DataValueField = "DevelopmentID";
