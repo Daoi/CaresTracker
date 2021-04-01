cf094e2792e041bf4cef6caff43ed25680f55af5
Daoi <klynch0092@gmail.com>
2021-04-01 19:11:38 -0400

    Temporary test page

---
+++ CaresTracker/TestPage.aspx.cs
@@ -0,0 +1,24 @@
+﻿using CaresTracker.DataAccess.DataAccessors;
+using CaresTracker.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CaresTracker
+{
+    public partial class TestPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            new CreateSQLUser(Session["User"] as CARESUser);
+        }
+    }
+}
