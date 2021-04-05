6abd78e003de54a589320c9ba193361a5519b0e3
Wilson Diaz <wdfiguereo@gmail.com>
2021-04-05 05:22:39 -0400

    refactor: rename report accessor

---
+++ CaresTracker/DataAccess/DataAccessors/ReportAccessors/ReportAccessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace CaresTracker.DataAccess.DataAccessors.ReportAccessors
+{
+    public class ReportAccessor
+    {
+        public ReportAccessor()
+        {
+            
+        }
+    }
+}
