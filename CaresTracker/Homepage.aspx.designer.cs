fab7b22f022d55ba5f7a53edda604a109e2f7984
Daoi <klynch0092@gmail.com>
2021-04-08 12:45:25 -0400

    vs files

--- CaresTracker/Homepage.aspx.designer.cs
+++ CaresTracker/Homepage.aspx.designer.cs
@@ -76,6 +76,15 @@ namespace CaresTracker {
         protected global::System.Web.UI.WebControls.GridView gvCompletedFollowUps;
         
         /// <summary>
+        /// lblEventMsg control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblEventMsg;
+        
+        /// <summary>
         /// gvEvents control.
         /// </summary>
         /// <remarks>
