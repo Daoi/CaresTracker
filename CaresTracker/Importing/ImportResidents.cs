2f0e4fdb9e1233c15e2c6a360af3acb8eb458734
Daoi <klynch0092@gmail.com>
2021-04-13 13:52:27 -0400

    Improve styling for errors

--- CaresTracker/Importing/ImportResidents.cs
+++ CaresTracker/Importing/ImportResidents.cs
@@ -11,11 +11,12 @@ namespace CaresTracker.Importing
 {
     public class ImportResidents
     {
-        public static (int inserted, List<int> errors) Import(List<ImportFile> residents, int devID, int regionID)
+        public static (int inserted, List<ImportError> errors) Import(List<ImportFile> residents, int devID, int regionID)
         {
             int successful = 0;
-            int row = 1;
-            List<int> rowsWithErrors = new List<int>();
+            int currentRow = 1;
+            List<ImportError> errors = new List<ImportError>();
+
 
             foreach (ImportFile record in residents)
             {
@@ -35,7 +36,7 @@ namespace CaresTracker.Importing
                 }
                 res.Gender = record.Gender;
                 res.Race = record.Race;
-                res.ResidentPhoneNumber = "000-000-0000"; //No address is provided in the PHA spreadsheet
+                res.ResidentPhoneNumber = "000-000-0000"; //No phone number is provided in the PHA spreadsheet
                 //House info
                 House home = new House();
                 home.Address = record.Address;
@@ -44,26 +45,47 @@ namespace CaresTracker.Importing
                 home.RegionID = regionID;
                 home.UnitNumber = string.IsNullOrWhiteSpace(record.SecondaryAddress) ? "N/A" : record.SecondaryAddress;
                 res.Home = home;
-                if (WriteResident(res))
-                    successful++;
-                else
-                    rowsWithErrors.Add(row);
-                row++;
+
+                string fullname = $"{res.ResidentFirstName} {res.ResidentLastName}";
+
+                int writeResult = WriteResident(res);
+                List<string> missingValues = ValidateValues(res);
+
+                if (missingValues.Count > 0)
+                {
+                    string s = string.Join("<br />", missingValues);
+                    errors.Add(new ImportError(currentRow, $"The following columns contained invalid values: <br /> {s}", fullname));
+                }
+                else {
+                
+                    if (writeResult == 1)
+                        successful++;
+                    else if (writeResult == 0)
+                    {
+                        errors.Add(new ImportError(currentRow, "Duplicate Resident", fullname));
+                    }
+                    else if (writeResult == -1)
+                    {
+                        errors.Add(new ImportError(currentRow, "SQL Error", fullname));
+                    }
+                }
+
+                currentRow++;
             }
 
-            return (successful, rowsWithErrors);
+            return (successful, errors);
         }
 
-        private static bool WriteResident(Resident newResident)
+        private static int WriteResident(Resident newResident)
         {
             try
             {
                 ResidentWriter RW = new ResidentWriter(newResident);
                 object AddResidentResult = RW.ExecuteCommand();
 
-                if (AddResidentResult == null) //If null Resident is NOT unique
+                if (AddResidentResult == DBNull.Value) //If null Resident is NOT unique
                 {
-                    return false;
+                    return 0; //Uniqueness error
                 }
 
                 House residentHouse = newResident.Home;
@@ -75,12 +97,44 @@ namespace CaresTracker.Importing
                 UpdateHouseID UHI = new UpdateHouseID();
                 UHI.ExecuteCommand(Convert.ToInt32(AddResidentResult), Convert.ToInt32(AddHouseResult));
 
-                return true;
+                return 1; //Succuess
             }
             catch (Exception e)
             {
-                return false;
+                return -1; //SQL Error?
             }
+        }
+
+        private static List<string> ValidateValues(Resident res)
+        {
+            List<string> invalidColumns = new List<string>();
+
+            if (string.IsNullOrWhiteSpace(res.ResidentFirstName))
+            {
+                invalidColumns.Add("Resident First Name(FIRST NAME)");
+            }
+
+            if (string.IsNullOrWhiteSpace(res.ResidentLastName))
+            {
+                invalidColumns.Add("Resident Last Name(LAST NAME)");
+            }
+
+            if (string.IsNullOrWhiteSpace(res.DateOfBirth))
+            {
+                invalidColumns.Add("Date of Birth(DATE OF BIRTH)");
+            }
+
+            if (string.IsNullOrWhiteSpace(res.Home.Address))
+            {
+                invalidColumns.Add("Resident Address(CURRENT ADDRESS-1)");
+            }
+
+            if (string.IsNullOrWhiteSpace(res.Home.ZipCode))
+            {
+                invalidColumns.Add("Zipcode(CURRENT ZIP CODE)");
+            }
+
+            return invalidColumns;
         }
 
     }
