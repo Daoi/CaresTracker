04b689b103e1dd508788b24ee44a269a0cb7e033
Daoi <klynch0092@gmail.com>
2021-04-19 16:49:52 -0400

    Set import status accessor

---
+++ CaresTracker/DataAccess/DataAccessors/ResidentAccessors/SetImported.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace CaresTracker.DataAccess.DataAccessors.ResidentAccessors
+{
+    public class SetImported : DataSupport, IData
+    {
+        public SetImported()
+        {
+            CommandText = "SetImported";
+            CommandType = CommandType.StoredProcedure;
+        }
+
+        public int ExecuteCommand(int residentID, bool imported)
+        {
+            Parameters = new MySqlParameter[2] { new MySqlParameter("ResidentID", residentID), new MySqlParameter("Imported", imported) };
+            ExecuteQuery eq = new ExecuteQuery();
+            return eq.ExecuteNonQuery(this);
+        }
+    }
+}
