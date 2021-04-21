7476b5888743be73b4120513d8c9c3d36c7f21b7
hachadorian <hachadorian98@gmail.com>
2021-04-21 00:14:10 -0400

    footer and chw management

--- CaresTracker/CaresTracker.Master.designer.cs
+++ CaresTracker/CaresTracker.Master.designer.cs
@@ -182,14 +182,5 @@ namespace CaresTracker {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
-        
-        /// <summary>
-        /// HyperLink1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
     }
 }
