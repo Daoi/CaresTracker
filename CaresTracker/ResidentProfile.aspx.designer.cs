28e1817883dde9afe9821400962e6ef36f52ccfe
Daoi <klynch0092@gmail.com>
2021-04-25 19:21:40 -0400

    vs files

--- CaresTracker/ResidentProfile.aspx.designer.cs
+++ CaresTracker/ResidentProfile.aspx.designer.cs
@@ -418,6 +418,24 @@ namespace CaresTracker {
         protected global::System.Web.UI.WebControls.TextBox tbAppointmentDate;
         
         /// <summary>
+        /// lblChronicHealth control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblChronicHealth;
+        
+        /// <summary>
+        /// cblChronicHealth control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBoxList cblChronicHealth;
+        
+        /// <summary>
         /// btnEditProfile control.
         /// </summary>
         /// <remarks>
