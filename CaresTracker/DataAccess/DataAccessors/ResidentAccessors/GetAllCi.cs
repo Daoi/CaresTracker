c6cd3e320d1e5ae2790493e77cc99c647e7b844e
Daoi <klynch0092@gmail.com>
2021-04-25 19:22:10 -0400

    Get list of CI

---
+++ CaresTracker/DataAccess/DataAccessors/ResidentAccessors/GetAllCi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace CaresTracker.DataAccess.DataAccessors.ResidentAccessors
+{
+    public class GetAllCi : DataSupport, IData
+    {
+        public GetAllCi()
+        {
+            CommandText = "GetAllCi";
+            CommandType = CommandType.StoredProcedure;
+        }
+
+        public DataTable RunCommand()
+        {
+            ExecuteQuery eq = new ExecuteQuery(); 
+            return eq.ExecuteAdapter(this);
+        }
+    }
+}
