2858d71eafd785364f658612154d6d7ebd7ee686
Wilson Diaz <wdfiguereo@gmail.com>
2021-04-24 20:13:13 -0400

    only show enabled services as options

---
+++ CaresTracker/DataAccess/DataAccessors/ServiceAccessors/GetEnabledServices.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace CaresTracker.DataAccess.DataAccessors.ServiceAccessors
+{
+    public class GetEnabledServices : DataSupport, IData
+    {
+        /// <summary>
+        /// Retrieve a DataTable containing all enabled services.
+        /// </summary>
+        public GetEnabledServices()
+        {
+            CommandText = "GetEnabledServices";
+            CommandType = CommandType.StoredProcedure;
+        }
+
+        public DataTable ExecuteCommand()
+        {
+            ExecuteQuery eq = new ExecuteQuery();
+            return eq.ExecuteAdapter(this);
+        }
+    }
+}
